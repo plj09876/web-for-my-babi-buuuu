efab101701dfd089a3881cecc0ec11d8351c005d
plj09876 <82640159+plj09876@users.noreply.github.com>
2022-12-11 21:19:06 +0900

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hi buuuu iuuu !",
+  text2: "Anh hỏi buuu chuyện này nhé ",
+  text3: "Buuuu có còn iu a khummmm ",
+  text4: "Nếu mà khum trả lời là anh sẽ mặc định là có đó nha ",
+  text5: "không bít ",
+  text6: "Cóa",
+  text7: "Lý do iu anh là rì hế ",
+  text8: "Gửi cho anh <3",
+  text9: "Vì em yêu anh không cần lý do hjhj đồ ngok",
+  text10: "I knew itt ^^ Illove you more than anything babii ",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Đầu năm sau a về a qua đón đi chơi =)))) Còn giờ thì chờ gì nữa mà ko nhắn tin cho anh  đi nèooo",
+  text12: "Oke hubby <3",
 };
 
 $(document).ready(function () {
